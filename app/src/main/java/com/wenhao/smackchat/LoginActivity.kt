7726fdddba2ccb5c12ebae3298430072a56755d3
wenhao <wenhao.z@live.com>
2019-03-05 22:20:50 -0800

    added login, create user, and main activities

---
+++ app/src/main/java/com/wenhao/smackchat/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.wenhao.smackchat
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginBtnOnClicked(view: View){
+
+    }
+
+    fun loginSignUpBtnOnClicked(view: View){
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
+    }
+}
